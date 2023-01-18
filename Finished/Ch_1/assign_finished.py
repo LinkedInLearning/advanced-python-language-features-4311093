d970a4dd210d9477895520ab99ed88b436ed5f87
joemarini <cjmarini68@gmail.com>
2023-01-17 22:49:20 -0800

    Add the assignment operator example

---
+++ Finished/Ch_1/assign_finished.py
@@ -0,0 +1,26 @@
+# Example file for Advanced Python: Language Features by Joe Marini
+# The assignment expression operator := (or the "walrus" operator)
+
+import pprint
+
+
+# regular assignment statements assign a value
+x = 5
+print(x)
+
+# the assignment operator is part of an expression
+(x := 10)
+print(x)
+
+# The assignment expression is useful for writing concise code
+while (thestr := input("value? ")) != "exit":
+    print(thestr)
+
+# The walrus operator can help reduce redundant function calls
+values = [12, 0, 10, 5, 9, 18, 41, 23, 30, 16, 18, 9, 18, 22]
+val_data = {
+    "length": (l := len(values)),
+    "total": (s := sum(values)),
+    "average": s / l
+}
+pprint.pp(val_data)
---
+++ Start/Ch_1/assign_start.py
@@ -0,0 +1,26 @@
+# Example file for Advanced Python: Language Features by Joe Marini
+# The assignment expression operator := (or the "walrus" operator)
+
+import pprint
+
+
+# regular assignment statements assign a value
+x = 5
+print(x)
+
+# TODO: the assignment operator is part of an expression
+(x := 10)
+print(x)
+
+# TODO: The assignment expression is useful for writing concise code
+while (thestr := input("value? ")) != "exit":
+    print(thestr)
+
+# TODO: The walrus operator can help reduce redundant function calls
+values = [12, 0, 10, 5, 9, 18, 41, 23, 30, 16, 18, 9, 18, 22]
+val_data = {
+    "length": len(values),
+    "total": sum(values),
+    "average": sum(values) / len(values)
+}
+pprint.pp(val_data)
